1a843ba4276e8141899b7e68738506d2887d1532
monish-parmar10 <monishparmar55@gmail.com>
2026-06-09 21:44:20 +0530

    Day-18

--- python45-/Day-14.py
+++ python45-/Day-14.py
@@ -1,57 +1,67 @@
 
-'''class,obj , prop, '''
+'''class,obj , prop/ using cls attributes and method(self) calling to get data through obj '''
 
-class ThisIsMyfirstClass:
+
+class ThisIsMyfirstClass: #<- class
     name = "monish" #property they are class attributes
     age = 20                   #they are class attributes
 
-    def getName(self): #method (self) refers to the current obj
+    def getName(self): #method (self) refers to the current obj(obj calls me i will receive)
         print()
 
 
 firstObject = ThisIsMyfirstClass()  #obj(instance of the class)
-print(firstObject) #o/p memory location of obj
+print(firstObject) #o/p Default string representation of the object (includes its type and an internal memory identifier).
 firstObject.getName() #to exicute get name method / method belongs to the object /ex - hey obj execute ur getname method then python send obj as self
 print(firstObject.name)   
 print(firstObject.age) 
 
 
-# class student: #class student blue print of obj ex- mould of sothing which give shape containng the 
-#     def __init__(dm):
-#         dm.name ="" #it means current obj has varble so called name
-#         dm.age = 0
-#         dm.gender = ""
-#         dm.grade = ""
-# #3 things obj does 1st memory(obj) is created in RAM 2nd cunstructor runs automatically 3rd address/reference of obj stored in variable -monish 
-# monish = student() #obj student as a constructor and it give the addresss where the data sstores and put into the monish
 
-# print(monish)
-# #updating obj data
-# monish.name = "monish parmar"
-# monish.age = 19
-# monish.gender = "male"
-# monish.grade = "1th"
-# print(monish.name)
-# print(monish.age)
-# print(monish.gender)
-# print(monish.grade)
+'''using constructor and updating data after creating obj '''
 
 
+class student: #class student blue print of obj ex- mould of something which give shape containng the 
+   
+   #auto run whenever obj is createds
+    def __init__(dm):
+        #(so called instance attributes)
+        dm.name ="" #it means current obj has var so called name
+        dm.age = 0   #it means current obj-> create var called name or age etc
+        dm.gender = ""
+        dm.grade = ""
+#3 things obj does 1st memory(obj) is created in RAM 2nd cunstructor runs automatically 3rd address/reference of obj stored in variable -monish 
+monish = student() #obj student as a constructor and it give the addresss where the data sstores and put into the monish
+                   #or for obj student() calls the class, which creates a new object and automatically runs __init__() to initialize it.
+print(monish)
+#updating obj data
+monish.name = "monish parmar"
+monish.age = 19
+monish.gender = "male"
+monish.grade = "1th" 
+print(monish.name)
+print(monish.age)
+print(monish.gender)
+print(monish.grade)
 
-# class Student:
-#     def __init__(dm, name, age , gender,grade):#constuct- initial value        #__init__ fix keyword in python for constructor
-#      dm.name = name
-#      dm.age = age
-#      dm.gender = gender
-#      dm.grade = grade         #class prop
+
+'''using constructor(initialise val) and method to print data '''
+
+
+class Student:
+    def __init__(self, name, age , gender,grade):#constuct- initial value        #__init__ fix keyword in python for constructor
+     self.name = name
+     self.age = age
+     self.gender = gender
+     self.grade = grade         #class prop
     
-#     def printDetails(dm):
-#         print("Name:",dm.name)
-#         print("age:",dm.age)
-#         print("gender",dm.gender)
-#         print("grade:",dm.grade)
+    def printDetails(self):
+        print("Name:",self.name)
+        print("age:",self.age)
+        print("gender",self.gender)
+        print("grade:",self.grade)
 
-# monish = Student("monish",20,"male","12th")#final val         #obj student as a constructor and it give the addresss where the data sstores and put into the monish
-# print(monish)
-
-# monish.printDetails()
+monish = Student("monish",20,"male","12th")#final val       #obj student as a constructor and it give the addresss where the data sstores and put into the monish
+print(monish) #it gives unreadable address or memory address of obj
+print(monish.__dict__) #dict returns a dictionary containing the object's instance attributes and their values.
+monish.printDetails()
---
+++ python45-/day-18.py
@@ -0,0 +1,177 @@
+'''use super class and super method '''
+# class super:
+#     def __init__(self,name):
+#         self.name = name
+#     def __str__(self): #end user also use repr method but str is for internal use 
+#         return "my name is " +self.name+"."
+# class Sub(super):
+#     def __init__(self,name):
+#         super.__init__(self,name)
+
+# obj = Sub("Andy")
+# print(obj)
+# print(obj.__dict__)#also custamise the dict format #use for explaining 
+    
+'''multiple inheritance'''
+
+class SuperA:
+    var_a = 10
+    def fun_a(self):
+        return 11
+class SuperB:
+    var_b = 20
+    def fun_b(self):
+        return 21
+    
+class Sub(SuperA,SuperB):
+    pass
+
+obj = Sub()
+
+print(obj.var_a,obj.fun_a())
+print(obj.var_b,obj.fun_b())
+
+'''multilevel inheritance'''
+# in python it checks from bottom-to-top
+class Level1: 
+    var = 100
+    def fun(self):
+        return 101
+class Level2(Level1):
+    var = 200
+    def fun(self):
+        return 201
+    
+class Level3(Level2):
+   pass
+   
+    # var = 303
+    # def fun(self):
+    #     return 333
+    
+
+obj = Level3()
+print(obj.var,obj.fun())
+
+
+#here about on same level left right
+'''multiple inheritance'''
+
+class Left:
+    var = "L"
+    var_left="LL"
+    def fun(self):
+        return "left"
+class Right:
+    var= "R"
+    var_right="RR"
+    def fun(self):
+        return "right"
+class Sub(Right,Left): #(left,right)
+    pass
+
+obj = Sub()
+print(obj.var,obj.var_left,obj.var_right,obj.fun())
+
+# MRO method resolution order precidence and order both in multilevel and multiple inheritance
+
+'''polymorphism one name many forms'''
+
+# class One: 
+#     def do_it(self):
+#         print("do it from one")
+
+#     def doanything(self):
+#         self.do_it()
+
+# class Two(One):
+#     def do_it(self):
+#         print("do_it  from Two")
+
+# # class Three(Two):
+# #       super.do_it(self):
+# #       print():  #try with super
+
+# one = One()
+# two = Two()
+# one.doanything()
+# two.doanything()   
+
+
+# def reciprocal(n):
+#     try:
+#         n = 1/n
+#     except ZeroDivisionError:
+#         print("ZeroDivisionError")
+#         return n
+#     else:
+#         print("evenrything is fine")
+#         return
+# print("-----------------")
+# print("reciprocal(2)",reciprocal(2))
+
+# print("-----------------")
+# print("reciprocal(0)",reciprocal(0))
+# print("-----------------")
+
+'''finally'''
+
+# def reciprocal(n):
+#     try:
+#         n = 1/n
+#     except ZeroDivisionError:
+#         print("ZeroDivisionError")
+#         return n
+#     else:
+#         print("evenrything is fine")
+#         return
+#     finally:
+#         print("its time to exit")
+#         return n
+
+
+# print("-----------------")
+# print("reciprocal(2)",reciprocal(2))
+
+# print("-----------------")
+# print("reciprocal(0)",reciprocal(0))
+# print("-----------------")
+
+
+
+
+# try:
+#     i = int("hello")
+# except Exception as e:
+
+#     print(e)
+#     print(e.__str__())
+
+
+
+'''create ur own exception/using base_keyword'''
+
+class MyZeroDivisionError(ZeroDivisionError):
+    pass
+def do_the_division(mine):
+    if mine:
+        raise MyZeroDivisionError("SOME WORSE NEWS")#raise is use to rise an error
+    else:
+        raise ZeroDivisionError("some bad news")
+
+do_the_division(False)
+do_the_division(True)
+
+
+
+# name = input("enter name")
+# def choice(name):
+#  try:
+#    name == str  
+#  except:
+#      print("this input is incorrect")
+     
+#  else:
+#      print(name)
+     
+
